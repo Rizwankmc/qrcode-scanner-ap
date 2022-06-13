45b35793824e29d1331285b3196b67143a3ced59
Rizwan Ahmad <32667679+Rizwankmc@users.noreply.github.com>
2022-06-13 19:48:58 +0530

    Add files via upload

--- routes.js
+++ routes.js
@@ -1,23 +1,40 @@
 import express from "express";
 
 const router = express.Router();
-
-let players = [];
+let players = [
+  { name: "Jhon", cards: [] },
+  { name: "Doe", cards: [] },
+  { name: "Justin", cards: [] },
+  { name: "Chris", cards: [] },
+  { name: "Adi", cards: [] },
+  { name: "Alex", cards: [] },
+  { name: "Ronaldo", cards: [] },
+  { name: "Rohan", cards: [] },
+];
 
 router.get("/distributeCards", async (req, res) => {
-  res.send({ code: 200, players });
+  res.send({ code: 200, players, currentPlayer: players[0] });
 });
 
 router.post("/scanCardData", async (req, res) => {
-  const { cardData } = req.body;
-  let player = {
-    label: players.length + 1,
-    card: cardData,
-  };
+  const { cardData, currentPlayer } = req.body;
+  if (cardData.length === 0) {
+    return res.send({ code: 200, players, currentPlayer });
+  }
 
-  players.push(player);
-
-  res.send({ code: 200, msg: "card added", players });
+  let player = players.find((el) => el.name === currentPlayer.name);
+  let index = players.findIndex((el) => el.name === currentPlayer.name);
+  player.cards.push(cardData);
+  players[index] = player;
+  if (player.cards.length === 2) {
+    if (players[index + 1]) {
+      res.send({ code: 200, players, currentPlayer: players[index + 1] });
+    } else {
+      res.send({ code: 200, msg: "scan complete", players });
+    }
+  } else {
+    res.send({ code: 200, players, currentPlayer: player });
+  }
 });
 
 export default router;
